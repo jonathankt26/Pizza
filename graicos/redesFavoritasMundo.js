5dfe39c6f70ce4a588ffbcdbab416be2a1e82598
jonathan <102986967+jonathankt26@users.noreply.github.com>
2024-10-17 11:31:01 -0300

    Add files via upload

---
+++ graicos/redesFavoritasMundo.js
@@ -0,0 +1,48 @@
+import { getCSS, criarGrafico } from "./common.js";
+
+async function redesFavoritasMundo() {
+  const url =
+    "https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json";
+  const res = await fetch(url);
+  const dados = await res.json();
+  const redes = Object.keys(dados);
+  const valores = Object.values(dados);
+
+  // Criar o gráfico de pizza
+  const data = [
+    {
+      values: valores,
+      labels: redes,
+      type: "pie",
+      textinfo: "label+percent",
+    },
+  ];
+
+  // Configurar o layout do gráfico
+
+  const layout = {
+    plot_bgcolor: getCSS("--bg-color"),
+    paper_bgcolor: getCSS("--bg-color"),
+    height: 700,
+    title: {
+      text: "Redes sociais que os usuários mais gostam",
+      x: 0,
+      font: {
+        color: getCSS("--primary-color"),
+        family: getCSS("--font"),
+        size: 30,
+      },
+    },
+    legend: {
+      font: {
+        color: getCSS("--primary-color"),
+        size: 16,
+      },
+    },
+  };
+
+  criarGrafico(data, layout)
+
+}
+
+redesFavoritasMundo();
